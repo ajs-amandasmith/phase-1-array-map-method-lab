7703aedcdafb69a9c745b51c22037e87971cda17
Amanda Smith <ajs.amandasmith@gmail.com>
2022-03-04 14:50:59 -0600

    commit tree d3df71abcab3a547087ca4037dcdab1dc0ee2a8d

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (sentence) {
+    const sentenceArrays = sentence.split(' ');
+    // console.log(sentenceArrays);
+    const wordArrays = sentenceArrays.map(function (words) {
+      // console.log('words upperCase:', words[0].toUpperCase() + words.slice(1));
+      return words[0].toUpperCase() + words.slice(1);
+    })
+    return wordArrays.join(' ');
+  });
 }
+
+
+console.log('titleCased:', titleCased());
+
+
+// Iterate over the input array
+// Split the sentences into words, by turning the strings into arrays
+// Iterate over the new arrays of words
+// Capitalize each word in the arrays (Can use .map() here)
+// Join the words into a string/sentence
